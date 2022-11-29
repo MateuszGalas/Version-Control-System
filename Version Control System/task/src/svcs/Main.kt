b6f7d4e9fcac3956b1588d77416240794e092baf
Mateusz Galaś <mateuszgalas94@gmail.com>
2022-11-29 17:50:07 +0100

    Actualization

--- Version Control System/task/src/svcs/Main.kt
+++ Version Control System/task/src/svcs/Main.kt
@@ -1,6 +1,53 @@
 package svcs
 
+import java.io.File
+
+fun config(name: String = "") {
+    val file = File("vcs\\config.txt")
+
+    if (!file.exists()) {
+        file.createNewFile()
+        file.writeText("Please, tell me who you are.")
+    }
+
+    if (name == "") {
+        println(file.readText())
+    } else {
+        file.writeText("The username is $name.")
+        println("The username is $name.")
+    }
+}
+
+fun add(name: String = "") {
+    val file = File("vcs\\index.txt")
+
+    if (!file.exists()) {
+        file.createNewFile()
+        println("Add a file to the index.")
+        return
+    }
+
+    if (name == "") {
+        println("Tracked files:\n${file.readText()}")
+    } else {
+        if (File("$name").exists()) {
+            println("The file '$name' is tracked." )
+            file.appendText(name + "\n")
+        } else {
+            println("Can't find '$name'.")
+        }
+    }
+}
+
 fun main(args: Array<String>) {
+
+    val file = File("vcs")
+
+    if (!file.exists()) {
+        file.mkdir()
+    }
+
+
     val help = "These are SVCS commands:\n" +
             "config     Get and set a username.\n" +
             "add        Add a file to the index.\n" +
@@ -9,10 +56,11 @@ fun main(args: Array<String>) {
             "checkout   Restore a file."
 
 
+
     when (args.firstOrNull()) {
-        "config" -> println("Get and set a username.")
+        "config" -> if (args.size > 1) config(args[1]) else config()
         "--help" -> println(help)
-        "add" -> println("Add a file to the index.")
+        "add" -> if (args.size > 1) add(args[1]) else add()
         "log" -> println("Show commit logs.")
         "commit" -> println("Save changes.")
         "checkout" -> println("Restore a file.")
